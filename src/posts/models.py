a9bcd225d806a36ffcc5b6f790524a4a1db9536e
kuskovan <techinek@gmail.com>
2021-06-10 15:01:52 +0300

    Delete previous db to prevent migrations from conflict. Add User based on Abstract user, add relevant FK for models

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,4 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+
+class User(AbstractUser):
+    """A user model with no custom fields"""
+    pass
+
+    def __str__(self):
+        return self.username
 
 
 class Post(models.Model):
@@ -8,8 +17,7 @@ class Post(models.Model):
     thumbnail = models.ImageField()
     publish_date = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
-
-    # author = models.Foreignkey()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
@@ -17,7 +25,7 @@ class Post(models.Model):
 
 class Comment(models.Model):
     """A comment model bound to the post"""
-    # user = models.ForeignKey()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
@@ -28,7 +36,7 @@ class Comment(models.Model):
 
 class PostView(models.Model):
     """A model for counting post views"""
-    # user = models.Foreignkey()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
 
@@ -38,7 +46,7 @@ class PostView(models.Model):
 
 class Like(models.Model):
     """A model for counting post likes"""
-    # user = models.ForeignKey()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
     def __str__(self):
