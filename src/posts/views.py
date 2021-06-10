6965e3f09cd48c8dfe740f5c8567992eac4a6688
kuskovan <techinek@gmail.com>
2021-06-10 15:56:01 +0300

    Add slug to Post model. Add generic views for CRUD operations with POST objects

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 
-# Create your views here.
+from .models import Post
+
+
+class PostListView(ListView):
+    """A class to display a list of posts"""
+    model = Post
+
+
+class PostDetailView(DetailView):
+    """A class to display a single post"""
+    model = Post
+
+
+class PostCreateView(CreateView):
+    """A class to create a single post"""
+    model = Post
+    fields = [
+        'title',
+        'content',
+        'thumbnail',
+        'author',
+        'slug',
+    ]
+
+
+class PostUpdateView(UpdateView):
+    """A class to update a single post"""
+    model = Post
+    fields = [
+        'title',
+        'content',
+        'thumbnail',
+        'author',
+        'slug',
+    ]
+
+class PostDeleteView(DeleteView):
+    """A class to delete a single post"""
+    model = Post
+    success_url = '/'
